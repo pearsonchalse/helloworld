559300a3bae728161dfc649fcfd6675ad47bfcf3
peterChalse <timer13171@gmail.com>
2024-02-25 17:53:06 +0900

    Bitmap Button Example [Operation] Press a button --> print the label in the terminal.

--- hello_wx.py
+++ hello_wx.py
@@ -15,27 +15,55 @@ class MyPanel(wx.Panel):
     def __init__(self,parent):
         super(MyPanel, self).__init__(parent)       
        
-        sizer = wx.BoxSizer(wx.VERTICAL)
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
         
-        self.label = wx.StaticText(self, label = "Hello, Toggle")
-        sizer.Add(self.label, 0 , wx.EXPAND) 
+        imgFile1 = "copy.png"
+        img1 = wx.Image(imgFile1, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        self.btn1 = wx.BitmapButton(
+            self, 
+            id=-1,
+            bitmap = img1,
+            size = (img1.GetWidth()+40,  img1.GetHeight()+40)
+            )
         
-        self.tBtn = wx.ToggleButton(self, label = "Click to On")    
-        self.tBtn.SetValue(True)
-        sizer.Add(self.tBtn, 0) 
-        self.tBtn.Bind(wx.EVT_TOGGLEBUTTON, self.onClickMe) 
-
-        self.SetSizer(sizer)
+        self.btn1.SetLabel("Copy")
+        hbox.Add(self.btn1,0,wx.ALIGN_CENTER)
+        self.btn1.Bind(wx.EVT_BUTTON, self.onClickMe)
+        
+        imgFile2 = "save.png"
+        img2 = wx.Image(imgFile2, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        self.btn2 = wx.BitmapButton(
+            self, 
+            id=-1,
+            bitmap = img2,
+            size = (img2.GetWidth()+40,  img2.GetHeight()+40)
+            )
+        
+        self.btn2.SetLabel("Save")
+        hbox.Add(self.btn2,0,wx.ALIGN_CENTER)
+        self.btn2.Bind(wx.EVT_BUTTON,self.onClickMe)
+        
+        imgFile3 = "folder.png"
+        img3 = wx.Image(imgFile3, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        self.btn3 = wx.BitmapButton(
+            self, 
+            id=-1,
+            bitmap = img3,
+            size = (img3.GetWidth()+40,  img3.GetHeight()+40)
+            )
+        
+        self.btn3.SetLabel("Folder")
+        hbox.Add(self.btn3,0,wx.ALIGN_CENTER)
+        self.btn3.Bind(wx.EVT_BUTTON, self.onClickMe)
+                
+        vbox.Add(hbox,1,wx.ALIGN_CENTER)
+        
+        self.SetSizer(vbox)
         
     def onClickMe(self, event): #event handling
-        state = event.GetEventObject().GetValue()
-        
-        if state == True:
-            self.label.SetLabelText("Off")
-            event.GetEventObject().SetLabel("Click To On")
-        else:
-            self.label.SetLabelText("On")
-            event.GetEventObject().SetLabel("Click To Off")
+        btn = event.GetEventObject().GetLabel()
+        print("Label is " + btn)
         
 class MyApp(wx.App):
     def OnInit(self):
